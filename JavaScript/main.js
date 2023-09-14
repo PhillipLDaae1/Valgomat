45e08f008459d7e1629f0ca165f8e38954475107
PhillipLDaae1 <e-phidaa@iskule.no>
2023-09-14 16:22:50 +0200

    Added scrolling functionality

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+let partiPoeng = {
+    rødt : 0,
+    sv : 0,
+    ap : 0,
+    sp : 0,
+    mdg : 0,
+    krf : 0,
+    venstre : 0,
+    høyre : 0,
+    frp : 0
+};
+
+nesteSpm = () => {
+    const x = 1050;
+    window.scrollBy({
+        top: x,
+        behavior: "smooth"
+    });
+}
