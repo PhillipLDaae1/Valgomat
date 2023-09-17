9df2866629163958d85367337b131352937ca56a
PhillipLDaae1 <e-phidaa@iskule.no>
2023-09-17 18:56:31 +0200

    Got the backend to work for adding points to the parties. Need to add more statements.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,19 +1,31 @@
 let partiPoeng = {
-    rødt : 0,
-    sv : 0,
-    ap : 0,
-    sp : 0,
-    mdg : 0,
-    krf : 0,
-    venstre : 0,
-    høyre : 0,
-    frp : 0
+    rødt: 0,
+    sv: 0,
+    ap: 0,
+    sp: 0,
+    mdg: 0,
+    krf: 0,
+    venstre: 0,
+    høyre: 0,
+    frp: 0
 };
 
-nesteSpm = () => {
+document.getElementById('valgomatForm').addEventListener('change', function (event) {
+    const selectedStatement = event.target.name; // Get the name of the selected statement
+    const selectedValue = event.target.value;
+    const selectedParties = selectedValue.split(',');
+
+    for (const party of selectedParties) {
+        if (partiPoeng.hasOwnProperty(party)) {
+            partiPoeng[party]++;
+        }
+    }
+
     const x = 1050;
     window.scrollBy({
         top: x,
         behavior: "smooth"
     });
-}
+
+    console.log(partiPoeng);
+});
